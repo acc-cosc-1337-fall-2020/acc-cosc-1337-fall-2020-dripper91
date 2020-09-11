e93fe72a656e0c186703a78ccf7a5ecb578385e5
Isaac <isaac.kittrell@g.austincc.edu>
2020-09-10 22:42:17 -0500

    completed homework 0

--- src/classwork/01_assign/types.cpp
+++ src/classwork/01_assign/types.cpp
@@ -1,5 +1,5 @@
 //Write the include statement for types.h here
-#include "types.h"
+#include <types.h>
 
 //DON'T ERASE THIS
 int sum_numbers(int num1, int num2)
@@ -7,7 +7,12 @@ int sum_numbers(int num1, int num2)
 	return num1 + num2;
 }
 
+int multiply_numbers(int num1)
+{
+	int num2 = 5;
 
+	return num1 * num2;
+}
 
 
 
