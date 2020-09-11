d6988e2ca29bf3d778fb105a0b20a22e399e92a7
Isaac <isaac.kittrell@g.austincc.edu>
2020-09-10 22:39:56 -0500

    Completed homework 0/classwork 01

--- src/classwork/01_assign/main.cpp
+++ src/classwork/01_assign/main.cpp
@@ -1,11 +1,24 @@
 //Write the include statement for types.h here
-
+#include <types.h>
 //Write include for capturing input from keyboard and displaying output to screen
-
+#include <iostream>
 
 
 int main() 
 {
+	int num;
+
+	std::cin >> num;
+
+	int result = multiply_numbers(num);
+
+	std::cout << "The multiplied product is " << result;
+
+	int num1 = 4;
+
+	result = multiply_numbers(num1);
+
+	std::cout << "\n\nThe multiplied product is " << result;
 
 	return 0;
 }
