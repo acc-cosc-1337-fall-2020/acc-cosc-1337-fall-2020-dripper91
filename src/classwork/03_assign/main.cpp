b62f66610c9308682ef5c17a0e5bda91abfd311b
Isaac <isaac.kittrell@g.austincc.edu>
2020-09-24 21:54:11 -0500

    Completed Homework 2

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -1,9 +1,19 @@
 //Write the include statement for decisions.h here
-
+#include <decision.h>
 //Write namespace using statements for cout and cin
+#include <iostream>
 
 int main() 
 {
+	int gradeNum;
+
+	std::cout << "Enter the number grade: ";  
+
+	std::cin >> gradeNum;
+
+	std::cout << "\nLetter grade using if/else is: " << get_letter_grade_using_if(gradeNum);
+
+	std::cout << "\nLetter grade using switch is: " << get_letter_grade_using_switch(gradeNum);
 
 	return 0;
 }
--- test/classwork_test/03_assign_test/decisions_tests.cpp
+++ test/classwork_test/03_assign_test/decisions_tests.cpp
@@ -1,8 +1,28 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
 #include "loops.h"
+#include <decision.h>
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test If/Else Statement")
+{
+	REQUIRE(get_letter_grade_using_if(96) == "A");
+}
+
+TEST_CASE("Test If/Else Statement 2")
+{
+	REQUIRE(get_letter_grade_using_if(78) == "C");
+}
+
+TEST_CASE("Test Switch Statement")
+{
+	REQUIRE(get_letter_grade_using_switch(69) == "D");
+}
+
+TEST_CASE("Test Switch Statement 2")
+{
+	REQUIRE(get_letter_grade_using_switch(117) == "ERROR");
+}
