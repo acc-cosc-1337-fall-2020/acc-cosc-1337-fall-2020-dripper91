c61f2984d67fff35fb71c960e2bf2bff49a50a27
Art Gonzalez <arturo.gonzalez@austincc.edu>
2020-09-28 21:14:55 -0500

    hw2 corrections

--- src/classwork/03_assign/main.cpp
+++ src/classwork/03_assign/main.cpp
@@ -11,9 +11,17 @@ int main()
 
 	std::cin >> gradeNum;
 
-	std::cout << "\nLetter grade using if/else is: " << get_letter_grade_using_if(gradeNum);
+	if(gradeNum <=0 && gradeNum >=0)
+	{
 
-	std::cout << "\nLetter grade using switch is: " << get_letter_grade_using_switch(gradeNum);
+		std::cout << "\nLetter grade using if/else is: " << get_letter_grade_using_if(gradeNum);
+
+		std::cout << "\nLetter grade using switch is: " << get_letter_grade_using_switch(gradeNum);
+	}
+	else
+	{
+		std::cout<<"Invalid grade";
+	}
 
 	return 0;
 }
