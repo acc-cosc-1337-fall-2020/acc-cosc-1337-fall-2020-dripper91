d5b2eaedcf7042a52221e34045bcfd8aa37bfc8e
Isaac <isaac.kittrell@g.austincc.edu>
2020-10-02 22:21:57 -0500

    HW3 main program and testing

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -9,8 +9,14 @@ Use a do while loop to prompt the user for
 a number, call the factorial function, and display the number's
 factorial.  Also, loop continues as long as user wants to.
 */
-int main() 
+int main()
 {
+	int num = 0;
+	
+	std::cout << "Enter a number to factorialize: ";
+	std::cin >> num;
+	
+	std::cout << "\nThe factorialized product is " << factorial(num);
 
 	return 0;
 }
--- test/classwork_test/04_assign_test/loops_tests.cpp
+++ test/classwork_test/04_assign_test/loops_tests.cpp
@@ -6,8 +6,18 @@ TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
-TEST_CASE("Veryify num 3 returns 6")
+TEST_CASE("Verify num 3 returns 6")
 {
 	REQUIRE(factorial(3)==6);
+}
+
+TEST_CASE("Verify num 4 returns 24")
+{
+	REQUIRE(factorial(4)==24);
+}
+
+TEST_CASE("Verify num 5 returns 120")
+{
+	REQUIRE(factorial(5)==120);
 }
 
