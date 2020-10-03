bb29f420c4eea00a7099c5d423edeaa0064dfcff
Isaac <isaac.kittrell@g.austincc.edu>
2020-10-02 23:03:28 -0500

    Changed HW3 main program

--- src/classwork/04_assign/main.cpp
+++ src/classwork/04_assign/main.cpp
@@ -12,11 +12,26 @@ factorial.  Also, loop continues as long as user wants to.
 int main()
 {
 	int num = 0;
+	char choice;
 	
-	std::cout << "Enter a number to factorialize: ";
-	std::cin >> num;
+	do
+	{
+		do
+		{
+			std::cout << "Enter a number from 1 to 10: ";
+			std::cin >> num;
+		} while (num < 1 || num > 10);
 	
-	std::cout << "\nThe factorialized product is " << factorial(num);
+		std::cout << "/nThe factorial of " << num << " is " << factorial(num);
+		std::cout << std::endl;
 
+		do
+		{
+			std::cout << "Enter another number? y/n: ";
+			std::cin >> choice;
+		} while (choice != 'y' && choice != 'n' && choice && 'Y' && choice != 'N');
+
+	} while (choice != 'n' && choice != 'N');
+	
 	return 0;
 }
