ce2a4cf6a4fad53c0b22866ec3759a0b1fed5dc6
Isaac Kittrell <isaac.kittrell@g.austincc.edu>
2020-11-19 23:37:57 -0600

    Homework 9 (push 2)

--- src/homework/tic_tac_toe/tic_tac_toe_data.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_data.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include "tic_tac_toe_data.h"
 #include "tic_tac_toe.h"
+#include "tic_tac_toe_3.h"
+#include "tic_tac_toe_4.h"
 
 void TicTacToeData::save_games(const std::vector<std::unique_ptr<TicTacToe>> &games)
 {
@@ -23,10 +25,31 @@ void TicTacToeData::save_games(const std::vector<std::unique_ptr<TicTacToe>> &ga
 
 std::vector<std::unique_ptr<TicTacToe>> TicTacToeData::get_games()
 {
+    std::vector<std::unique_ptr<TicTacToe>> games;
     std::ifstream inFile;
     inFile.open("gamelist.txt");
     std::string line;
-    std::vector<std::string> pegs;
 
-    while(getline())
+    while(getline(inFile, line))
+    {
+        std::vector<std::string> pegs;
+
+        for(unsigned i = 0; i < line.size() - 1; i++)
+        {
+            std::string string(1, line[i]);
+            pegs.push_back(string);
+        }
+
+        std::string winner(1, line[line.size()-1]);
+        std::unique_ptr<TicTacToe> game;
+
+        if(pegs.size()==9)
+            game = std::make_unique<TicTacToe3>(pegs, winner);
+        else if(pegs.size()==16)
+            game = std::make_unique<TicTacToe4>(pegs, winner);
+
+        games.push_back(std::move(game));
+    }
+
+    return games;
 }
